962dbf1f8d62fc5b62181709d740153161c6c14f
Brad <brad@brad.tw>
2016-10-04 20:39:18 +0800

    v3

--- app/src/main/java/brad/tw/mynetworktest/MainActivity.java
+++ app/src/main/java/brad/tw/mynetworktest/MainActivity.java
@@ -1,20 +1,28 @@
 package brad.tw.mynetworktest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText input;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        input = (EditText)findViewById(R.id.inputData);
     }
 
     public void udpSend(View v){
@@ -27,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendUDP(){
-        byte[] buf = "Hello, World".getBytes();
+        byte[] buf = input.getText().toString().getBytes();
         try {
             DatagramSocket socket =
                     new DatagramSocket();
@@ -39,9 +47,19 @@ public class MainActivity extends AppCompatActivity {
                             8888);
             socket.send(packet);
             socket.close();
+
             Log.v("brad", "Send UDP OK");
         }catch(Exception ee){
             Log.v("brad", ee.toString());
+        }
+        handler.sendEmptyMessage(0);
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            input.setText("");
         }
     }
 
